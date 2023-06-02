102980473f3f6dddb13547f9edccfef32719af7d
Melliim <ibrahim_meleha@hotmail.com>
2023-06-02 03:08:43 +0200

    add generated SpringBoot and first Controller GuK HTML

---
+++ src/main/java/QuickChange/GuKMelde/App/GuKMeldeAppApplication.java
@@ -0,0 +1,13 @@
+package QuickChange.GuKMelde.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuKMeldeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuKMeldeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/QuickChange/GuKMelde/App/GuKMeldeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package QuickChange.GuKMelde.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuKMeldeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
