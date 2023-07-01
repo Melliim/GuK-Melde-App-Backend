b27b6dbc6b4db03808e3a1713743f0bdb8449018
Jascha Steudler <jascha.steudler@gmail.com>
2023-07-01 12:24:02 +0200

    Upload + Krankmeldung

--- src/main/java/QuickChange/GuKMelde/App/Repos/FileInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package QuickChange.GuKMelde.App.Repos;
-
-
-import QuickChange.GuKMelde.App.Entities.FileInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
-}
-
